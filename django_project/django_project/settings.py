593dba92b544c4fdce2a9101e7ce07cb0faa0ccd
Ravuri Poojitha <ravuri.poojitha123@gmail.com>
2021-06-29 20:59:20 +0530

    sqlite3

--- django_project/django_project/settings.py
+++ django_project/django_project/settings.py
@@ -78,7 +78,7 @@ WSGI_APPLICATION = 'django_project.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
-
+'''
 DATABASES = {
      'default': {
         'ENGINE': 'django.db.backends.mysql',
@@ -87,6 +87,13 @@ DATABASES = {
         'PASSWORD': 'root',
         'HOST': '127.0.0.1',   # Or an IP Address that your DB is hosted on
         'PORT': '3306',
+    }
+}'''
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
